3a817343c01a704bafe90263de04c4ca75d61ba0
Marc Löwenstein <Marc.Lowenstein@gmail.com>
2023-10-24 23:43:13 +0200

    Finetuning

--- VariabelArray/VariabelArray.py
+++ VariabelArray/VariabelArray.py
@@ -1,39 +1,35 @@
-boolTurtle = False
 try:
     import turtle as tt
-except ImportError: #import turtle mislukt
-    boolTurtle = True
-boolNumpy = False
+except ImportError: #import turtle failed
+    print("This software uses turtle.")
+    exit()
 try:
     import numpy as np
-except ImportError: #import numpy mislukt
-    boolNumpy = True
+except ImportError: #import numpy failed
+    print("This software uses numpy.")
+    exit()
 
-if boolTurtle: #import turtle mislukt
-    rows = int(input('Hoeveel rijen en kolommen in de array?\n'))
-else:
-    rows = int(tt.numinput("Invoer", "Hoeveel rijen en kolommen in de array", default=10, minval=0, maxval=60))
+#init
+rows = int(tt.numinput("Input", "How many rows en columns in the array", default=10, minval=0, maxval=60))
 cols = rows
-if boolNumpy: #import numpy mislukt
-    arr = [[0 for i in range(cols)] for j in range(rows)]
-else:
-    arr = np.zeros((rows, cols), dtype=int)
+arr = np.zeros((rows, cols), dtype=int)
 
+#process
 for rowWaarde in range(rows):
     for colWaarde in range(cols):
-        if (rowWaarde % 2): # oneven rijen
-            if not (colWaarde % 2): # even kolommen
+        changeValue = False
+        if (rowWaarde % 2): # odd rows
+            if not (colWaarde % 2): # even columns
                 if (colWaarde < rowWaarde):
-                    arr[rowWaarde][colWaarde] = 1
-        else: # even rijen
-            if (colWaarde % 2): # oneven kolommen
+                    changeValue = True
+        else: # even rows
+            if (colWaarde % 2): # odd columns
                 if (colWaarde > rowWaarde):
-                    arr[rowWaarde][colWaarde] = 1
-            else: # even kolommen
-                arr[rowWaarde][colWaarde] = 1
-                
-if boolNumpy: #import numpy mislukt
-    for row in arr:
-        print(row)
-else:
-    print (arr)
+                    changeValue = True
+            else: # even columns
+                changeValue = True
+        if (changeValue):
+            arr[rowWaarde][colWaarde] = 1
+ 
+#close                
+print (arr)
