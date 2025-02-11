858f6c89ffa90e23ebedb54dc80ecdcf027dc6d7
Marc Löwenstein <Marc.Lowenstein@gmail.com>
2025-02-11 01:58:27 +0100

    Verbeter van foutmelding

--- VariabelArray/VariabelArray.py
+++ VariabelArray/VariabelArray.py
@@ -1,13 +1,13 @@
-try: #import turtle
-    import turtle as tt
-except ImportError: #import turtle failed
-    print("This software uses turtle.")
-    exit()
-try: #import numpy
-    import numpy as np
-except ImportError: #import numpy failed
-    print("This software uses numpy.")
-    exit()
+try: 
+    import turtle as tt #import turtle
+except ImportError as e: #import turtle failed
+    print(f"Fatal error: {e}")
+    exit(1)
+try: 
+    import numpy as np #import numpy
+except ImportError as e: #import numpy failed
+    print(f"Fatal error: {e}")
+    exit(1)
 
 #init
 rows = cols = int(tt.numinput("Input", "How many rows en columns in the array", default=10, minval=0, maxval=60))
