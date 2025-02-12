034be713e679873084b4abf64f88b890ed02c891
Marc Löwenstein <TheStoneLion@users.noreply.github.com>
2025-02-12 19:58:08 +0100

    Update VariabelArray.py
    variablename changed

--- VariabelArray/VariabelArray.py
+++ VariabelArray/VariabelArray.py
@@ -10,11 +10,11 @@ except ImportError as e: #import numpy failed
     exit(1)
 
 #init
-squareSize = int(tt.numinput("Input", "How big must the square be?", default=10, minval=0, maxval=60))
+square = int(tt.numinput("Input", "How big must the square be?", default=10, minval=0, maxval=60))
 
 #process
-for row in range(squareSize):
-    rowArray = np.empty((squareSize), dtype=int)
-    for col in range(squareSize):
-        rowArray[col] = 0 if min(row, col) % 2 else 1     
-    print (rowArray) #print
+for row in range(square):
+    colArray = np.empty((square), dtype=int)
+    for col in range(square):
+        colArray[col] = 0 if min(row, col) % 2 else 1     
+    print (colArray) #print
