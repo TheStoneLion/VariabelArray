85fc920b67aa064a425ef94cf72c3214c47847c2
Marc Löwenstein <Marc.Lowenstein@gmail.com>
2025-02-14 19:13:39 +0100

    Updated

--- VariabelArray/VariabelArray.py
+++ VariabelArray/VariabelArray.py
@@ -1,8 +1,8 @@
+ttBool = True
 try: 
     import turtle as tt #import turtle
 except ImportError as e: #import turtle failed
-    print(f"Fatal error: {e}")
-    exit(1)
+    ttBool = False
 try: 
     import numpy as np #import numpy
 except ImportError as e: #import numpy failed
@@ -10,14 +10,18 @@ except ImportError as e: #import numpy failed
     exit(1)
 
 #init
-square = tt.numinput("Input", "How big must the square be?", default=10, minval=1, maxval=60)
-if square is None: #Check if input is cancelled
-    print("Input cancelled; End of program")
-    exit(0)
+if ttBool:
+    square = tt.numinput("Input", "How big must the square be?", default=10, minval=1, maxval=60)
+    if square is None: #Check if input is cancelled
+        print("Input cancelled; End of program")
+        exit(0)
+    square = int(square)
+else:
+    square = 10
 
 #process
-for row in range(int(square)):
-    colArray = np.empty((int(square)), dtype=int)
-    for col in range(int(square)):
+for row in range(square):
+    colArray = np.empty((square), dtype=int)
+    for col in range(square):
         colArray[col] = 0 if min(row, col) % 2 else 1     
     print (colArray) #print
