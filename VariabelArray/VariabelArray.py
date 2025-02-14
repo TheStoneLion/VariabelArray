5e7d27accb4a7d1c45bade2cdd4e2bb63673f3bd
Marc Löwenstein <Marc.Lowenstein@gmail.com>
2025-02-14 23:50:06 +0100

    With turtle drawer

--- VariabelArray/VariabelArray.py
+++ VariabelArray/VariabelArray.py
@@ -1,3 +1,5 @@
+import time
+import matplotlib_inline as plt
 ttBool = True
 try: 
     import turtle as tt #import turtle
@@ -20,21 +22,60 @@ def validNumber(value): #check input
         print ("Value must be numeric")
         exit(0)
 
+def draw_square(color):
+    tt.pendown()
+    if color == 1:
+        tt.pencolor("black")
+        tt.fillcolor("black")
+    else:
+        tt.pencolor("white")
+        tt.fillcolor("white")
+    tt.begin_fill()
+    for i in range(4):
+        tt.forward(blocksize)
+        tt.right(90)
+    tt.end_fill()
+    tt.forward(blocksize)
+
+
 #init
+blocksize = 100
+
 if ttBool:
+    tt.penup()
+    tt.screensize(1000,1000)
+    tt.goto(-tt.screensize()[0]/2,tt.screensize()[1]/2)
+    tt.bgcolor("green")
+    tt.speed(0)
+    tt.hideturtle()
+
     square = tt.numinput("Input", "How big must the square be?", default=10, minval=1, maxval=60)
     if square is None: #Check if input is cancelled
         print("Input cancelled; End of program")
         exit(0)
     square = int(square)
+    blocksize = 1000/square
 else:
     square = validNumber(input("Enter a value from 1 to 60:\n"))
+
 
 #process
 for row in range(square):
     colArray = np.empty((square), dtype=int)
     for col in range(square):
-        colArray[col] = 0 if min(row, col) % 2 else 1     
-    print (colArray) #print
+        colArray[col] = 0 if min(row, col) % 2 else 1    
+        if ttBool:
+            draw_square(colArray[col])
+    if ttBool:
+        tt.penup()
+        tt.left(180)
+        tt.forward(blocksize*square)
+        tt.left(90)
+        tt.forward(blocksize)
+        tt.left(90)
 
+    else:
+        print (colArray) #print
+
+time.sleep(15)
 
