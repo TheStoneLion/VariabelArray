4e1b472c07285a0ca64d77aa5ee25e4b1a8fac0a
Marc Löwenstein <Marc.Lowenstein@gmail.com>
2025-02-14 23:16:09 +0100

    Updates

--- VariabelArray/VariabelArray.py
+++ VariabelArray/VariabelArray.py
@@ -1,6 +1,6 @@
 ttBool = True
 try: 
-    import turtle as tt #import turtle
+    import turtles as tt #import turtle
 except ImportError as e: #import turtle failed
     ttBool = False
 try: 
@@ -8,6 +8,17 @@ try:
 except ImportError as e: #import numpy failed
     print(f"Fatal error: {e}")
     exit(1)
+
+def validNumber(value): #check input
+    if value.isnumeric():
+        if int(value) in range(1, 61):
+            return(int(value))            
+        else:
+            print ("Invalid value")
+            exit(0)
+    else:
+        print ("Value must be numeric")
+        exit(0)
 
 #init
 if ttBool:
@@ -17,7 +28,7 @@ if ttBool:
         exit(0)
     square = int(square)
 else:
-    square = 10
+    square = validNumber(input("Enter a value from 1 to 60:\n"))
 
 #process
 for row in range(square):
@@ -25,3 +36,5 @@ for row in range(square):
     for col in range(square):
         colArray[col] = 0 if min(row, col) % 2 else 1     
     print (colArray) #print
+
+
